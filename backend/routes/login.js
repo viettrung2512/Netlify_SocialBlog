a318a061a9a0a7b2ad2c0f11e9b36d1b591ef9b4
NguyenVietTrung <117735066+viettrung2512@users.noreply.github.com>
2025-05-27 14:31:07 +0700

    loginUser

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/authController');
 
 // POST /login
 router.post('/', authController.login);
+router.post('/', authController.googleLogin);
 
 module.exports = router;
