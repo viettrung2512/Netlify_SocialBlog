7f7b84f636da5a703cbc840f7e7a420b69219058
NguyenVietTrung <117735066+viettrung2512@users.noreply.github.com>
2025-05-27 14:34:56 +0700

    add new file loginGoogle

--- backend/routes/loginGoogle.js
+++ backend/routes/loginGoogle.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// POST /login
-router.post('/', authController.login);
+router.post('/', authController.googleLogin);
 
 module.exports = router;
