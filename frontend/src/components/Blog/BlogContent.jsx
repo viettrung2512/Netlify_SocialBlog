3a20e807ba60fd7d16f3ea57b1bbd2f1df81e5b6
NguyenVietTrung <117735066+viettrung2512@users.noreply.github.com>
2025-05-28 14:27:02 +0700

    ..

--- frontend/src/components/Blog/BlogContent.jsx
+++ frontend/src/components/Blog/BlogContent.jsx
@@ -244,12 +244,12 @@ useEffect(() => {
                   </a>
                 </div>
                 <h3 className="text-xl font-bold text-gray-900">
-                  <a
-                    href={`/profile/${blog.author.id}`}
-                    className="hover:text-blue-600 transition-colors"
-                  >
-                    {blog.author.name}
-                  </a>
+                <a
+                  href={`/profile/${blog.author._id}`}
+                  className="hover:text-blue-600 transition-colors"
+                >
+                  {blog.author.name}
+                </a>
                 </h3>
                 <p className="text-gray-500 text-sm mt-1">
                   {blog.author.email}
@@ -257,7 +257,7 @@ useEffect(() => {
 
                 <div className="mt-6 pt-6 border-t border-gray-100">
                   <a
-                    href={`/profile/${blog.author.id}`}
+                    href={`/profile/${blog.author._id}`}
                     className="inline-flex items-center px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
                   >
                     <User className="h-4 w-4 mr-2" />
